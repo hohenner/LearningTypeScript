17e8461600b8f5a8b73f5d2d2d6a4d1d2c367fae
Andy Hohenner <ahohenner@mdsol.com>
2021-09-13 13:47:57 -0700

    exercise 02-04

--- Ch02/02_04/src/model.ts
+++ Ch02/02_04/src/model.ts
@@ -2,7 +2,8 @@ let displayName: string = "Jess's standing desk";
 let inventoryType: string = "furniture";
 let trackingNumber: string = "FD123455";
 let createDate: Date = new Date();
-let originalCost: any = 425;
+type Cost = number | string;
+let originalCost: Cost = 425;
 originalCost = "A LOT of money!";
 
 enum InventoryItemType {
